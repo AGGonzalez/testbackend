4f65528c636c6d67f24909df819ea9852c4cfb73
Antonio González <agonzalez@profile.es>
2017-03-13 09:40:22 +0100

    Initial Comiit

---
+++ src/main/java/com/profile/es/TestbackendApplication.java
@@ -0,0 +1,12 @@
+package com.profile.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestbackendApplication.class, args);
+	}
+}
